44906f362baaf97b6ca8eefad175c6908690394c
Shreyasgeetha <shreyasgeetha@gmail.com>
2023-12-19 17:03:42 +0530

    fromto

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import BasicTween from "./GSAP3Express/Tweens/BasicTween";
+import FromTo from "./GSAP3Express/Tweens/FromTo";
 
 function App() {
   return (
     <div>
-      <BasicTween />
+      {/* <BasicTween /> */}
+      <FromTo />
     </div>
   );
 }
---
+++ src/GSAP3Express/Tweens/FromTo.jsx
@@ -0,0 +1,30 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+
+const FromTo = () => {
+  useGSAP(() => {
+    // Step 1
+    // gsap.from(".fred", { x: 400, y: 200, scale: 2, duration: 3 });
+    //Step 2
+    // gsap.fromTo(".fred", { x: 700, y: 400 }, { x: 400, y: 200, duration: 3 });
+    // step 3 - trying with opacity, useful in website - making things appear from nowhere
+    gsap.fromTo(
+      ".fred",
+      { x: 700, y: 400, opacity: 0 },
+      { x: 400, y: 200, opacity: 1, duration: 1 },
+    );
+  }, []);
+  return (
+    <div>
+      <img
+        className="fred"
+        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32887/fred.svg"
+        alt="description"
+        style={{ width: 150 }}
+      />
+    </div>
+  );
+};
+
+export default FromTo;
